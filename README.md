90032b2d94b15dd171f01e35b966da98812ffeee
codelitt <littlewoodcody@gmail.com>
2013-10-09 00:18:12 -0300

    added views, slim, bootstrap, and assetpack

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ codelitt
 ========
 
 Repo for http://www.codelitt.com
+
+Colour for brand: 0093D0
--- codelitt.rb
+++ codelitt.rb
@@ -1,9 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/base'
-
+require 'sinatra/assetpack'
+require 'slim'
 class Codelitt < Sinatra::Base
-  
+  set :root, File.dirname(__FILE__)
+
+  register Sinatra::AssetPack
+
+  enable :inline_templates
+
+  assets do
+    serve '/js',     from: 'app/js'        # Default
+    serve '/css',    from: 'app/css'       # Default
+    serve '/image', from: 'app/image'    # Default
+
+    css :cssapp, [
+      '/css/*.css'
+    ]
+              
+    js :jsapp, [
+      '/js/*.js'
+    ]
+            
+    css_compression :simple
+    js_compression :jsmin
+  end
+              
+                                                 
   get '/' do
     slim :index
   end
